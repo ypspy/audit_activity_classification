2e9627bc161ab199802c66ed5c4418c48783b57e
LAPTOP-5H515OKA\yoonseok <yoontjr@gmail.com>
2021-07-25 11:12:15 +0900

    미등록 단어 분석 고려한 조성사항 반영

--- analytics.py
+++ analytics.py
@@ -3,6 +3,8 @@
 Created on Tue Jul 13 00:48:39 2021
 
 @author: yoonseok
+
+1. Python Komoran 사용자 사전 추가 https://lovit.github.io/nlp/2018/04/06/komoran/
 """
 
 import pandas as pd
@@ -12,6 +14,8 @@ import random
 import os
 
 from ckonlpy.tag import Twitter, Postprocessor
+from konlpy.tag import Komoran
+from tqdm import tqdm
 
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split
@@ -25,22 +29,21 @@ from sklearn.pipeline import Pipeline
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 
-def tokenizeDocuments(documentColumn, model, spacing=False):
+def tokenizeDocuments(documentColumn, model, stopWords, desc = None):
     
     container = []
-    for string in documentColumn:
-        string = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ·!』\\‘|\(\)\[\]\<\>`\'…》]', '', string)  # 특수문자 제거
-        string = re.sub('\w*[0-9]\w*', '', string)  # 숫자 제거
+    for string in tqdm(documentColumn, desc=desc):
+        string = re.sub('[-=+,#/\?:^$.@*\"“”※~&%ⅰⅱⅲ○●ㆍ°’『!』」\\‘|\(\)\[\]\<\>`\'…》]', '', string)  # 특수문자 제거
+        # string = re.sub('\w*[0-9]\w*', '', string)  # 숫자 제거
         string = re.sub('\w*[a-zA-Z]\w*', '', string)  # 알파벳 제거
         string = string.strip()  # 문서 앞뒤 공백 제거
-        if spacing:
-            string = ''.join(string.split())  # Remove whitespace 
-        else:
-            string = ' '.join(string.split())  # Replace Multiple whitespace into one
-        tokenList = model(string)
-        for i in tokenList:
+        string = ' '.join(string.split())  # Replace Multiple whitespace into one
+        
+        tokenList = model(string)  # 형태소분석기로 문서 형태소 추출
+        for i in tokenList:  # 추출된 형태소 중 불용어 제거
             if i in stopwords:
                 tokenList.remove(i)
+        
         container.append(tokenList)
     return container
 
@@ -122,64 +125,67 @@ def vectorizeDoc2Vec(df, model="doc2Vec"):
 os.chdir(r"C:\analytics")
 
 # Tune NLP Tool
-twitter = Twitter()
+tokenizer_tuned = Komoran(userdic='Ngramdictionary.txt')
+tokenizer = Komoran()
 
 dictionary = pd.read_csv("dataset6.dictionary.csv", names=["noun"])
-nounList = dictionary.noun.to_list()
-twitter.add_dictionary(nounList, 'Noun')
+# nounList = dictionary.noun.to_list()
+# twitter.add_dictionary(nounList, 'Noun')
 
 stopWord = pd.read_csv("dataset7.stopwords.csv", names=["stopword"])
 stopwords = stopWord.stopword.to_list()
-postprocessor = Postprocessor(twitter, stopwords = stopwords)
+# postprocessor = Postprocessor(tokenizer.nouns, stopwords = stopwords)
+# postprocessor_tuned = Postprocessor(tokenizer_tuned.nouns, stopwords = stopwords)
 
 # import preprocessed dataset
 df = pd.read_excel("dataset3.preprocessed(2017-2019).xlsx", sheet_name="data")
 
 # preprocessing
-df["twitter"] = tokenizeDocuments(df["documents"], postprocessor.pos)
-
+df["token"] = tokenizeDocuments(df["documents"], tokenizer.nouns, stopwords, desc="사용자 사전 미반영")
+df["token_tuned"] = tokenizeDocuments(df["documents"], tokenizer_tuned.nouns, stopwords, desc="사용자 사전 반영")
+                                       
 # drop blank cells
-drop_index = df[df['document'] == ''].index
-df = df.drop(drop_index)
+# drop_index = df[df['document'] == ''].index
+# df = df.drop(drop_index)
 
-# sample and export training data 
-dfLabel = df['document'].sample(n=1000, random_state=1)
-dfLabel.to_excel("dataset4.trainingData.xlsx") 
+# # sample and export training data 
+# dfLabel = df['document'].sample(n=1000, random_state=1)
+# dfLabel.to_excel("dataset4.trainingData.xlsx") 
 
-# Word Embedding - Counter
-countVec = CountVectorizer()
-countVecMatrix = countVec.fit_transform(df["document"])
+# # Word Embedding - Counter
+# countVec = CountVectorizer()
+# countVecMatrix = countVec.fit_transform(df["document"])
 
-# Word Embedding - TF-IDF
-tfidfVec = TfidfVectorizer()
-tfidfVecMatrix = tfidfVec.fit_transform(df["document"])
+# # Word Embedding - TF-IDF
+# tfidfVec = TfidfVectorizer()
+# tfidfVecMatrix = tfidfVec.fit_transform(df["document"])
 
-# Word Embedding - LDA
-ldaVec = LatentDirichletAllocation(n_components=10, random_state=1)
-ldaVecMatrix = ldaVec.fit_transform(countVecMatrix)
+# # Word Embedding - LDA
+# ldaVec = LatentDirichletAllocation(n_components=10, random_state=1)
+# ldaVecMatrix = ldaVec.fit_transform(countVecMatrix)
 
-# Word Embedding - Doc2Vec 
-doc2Vec = Doc2Vec()
-dfDoc2Vec = df
-train_corpus = transformDF2Corpus(dfDoc2Vec)
-doc2Vec.build_vocab(train_corpus)
-doc2Vec.train(train_corpus, total_examples=doc2Vec.corpus_count, epochs=doc2Vec.epochs)
-doc2VecMatrix = vectorizeDoc2Vec(df)
+# # Word Embedding - Doc2Vec 
+# doc2Vec = Doc2Vec()
+# dfDoc2Vec = df
+# train_corpus = transformDF2Corpus(dfDoc2Vec)
+# doc2Vec.build_vocab(train_corpus)
+# doc2Vec.train(train_corpus, total_examples=doc2Vec.corpus_count, epochs=doc2Vec.epochs)
+# doc2VecMatrix = vectorizeDoc2Vec(df)
 
-# Set Pipeline - NB
-NB = MultinomialNB()
+# # Set Pipeline - NB
+# NB = MultinomialNB()
 
-Pipeline_NB_TfIdf = Pipeline([
-    ('vect', tfidfVec),
-    ('clf', NB)
- 	])
+# Pipeline_NB_TfIdf = Pipeline([
+#     ('vect', tfidfVec),
+#     ('clf', NB)
+#  	])
 
-Pipeline_NB_LDA = Pipeline([
-    ('vect', ldaVec),
-    ('clf', NB)
- 	])
+# Pipeline_NB_LDA = Pipeline([
+#     ('vect', ldaVec),
+#     ('clf', NB)
+#  	])
 
-Pipeline_NB_Doc2Vec = Pipeline([
-    ('vect', doc2Vec),
-    ('clf', NB)
- 	])
+# Pipeline_NB_Doc2Vec = Pipeline([
+#     ('vect', doc2Vec),
+#     ('clf', NB)
+#  	])
--- auditingWordExtract/ngramExtractor.py
+++ auditingWordExtract/ngramExtractor.py
@@ -14,8 +14,9 @@ import os
 import pandas as pd
 import re
 from collections import defaultdict
+from tqdm import tqdm
 
-def doPreprocess(documentColumn):
+def doPreprocess(documentColumn, model, stopWords, desc = None):
     """
     Dataframe에서 문서열을 받아와서, 특수문자, 숫자, 알파벳, 불필요한 공백을 제거
 
@@ -31,23 +32,22 @@ def doPreprocess(documentColumn):
 
     """
     container = []
-    for string in documentColumn:
+    for string in tqdm(documentColumn, desc=desc):
         string = re.sub('[-=+,#/\?:^$.@*\"“”※~&%ⅰⅱⅲ○●ㆍ°’『!』」\\‘|\(\)\[\]\<\>`\'…》]', '', string)  # 특수문자 제거
-        string = re.sub('\w*[0-9]\w*', '', string)  # 숫자 제거
+        # string = re.sub('\w*[0-9]\w*', '', string)  # 숫자 제거
         string = re.sub('\w*[a-zA-Z]\w*', '', string)  # 알파벳 제거
         string = string.strip()  # 문서 앞뒤 공백 제거
         string = ' '.join(string.split())  # Replace Multiple whitespace into one
         
+        tokenList = model(string)  # 형태소분석기로 문서 형태소 추출
+        for i in tokenList:  # 추출된 형태소 중 불용어 제거
+            if i in stopwords:
+                tokenList.remove(i)
+        string = ' '.join(tokenList)
         container.append(string)
     return container
 
-def to_ngrams(words, n):
-    ngrams = []
-    for b in range(0, len(words) - n + 1):
-        ngrams.append(tuple(words[b:b+n]))
-    return ngrams
-
-def get_ngram_counter(docs, min_count=10, n_range=(1,3)):
+def get_ngram_counter(docs, model, min_count=10, n_range=(1,3)):
 
     def to_ngrams(words, n):
         ngrams = []
@@ -57,8 +57,8 @@ def get_ngram_counter(docs, min_count=10, n_range=(1,3)):
 
     n_begin, n_end = n_range
     ngram_counter = defaultdict(int)
-    for doc in docs:
-        words = komoran.nouns(doc)  # pos(doc, join=True)
+    for doc in docs:  # https://calmcode.io/tqdm/nested-loops.html
+        words = model(doc)  # pos(doc, join=True)
         for n in range(n_begin, n_end + 1):
             for ngram in to_ngrams(words, n):
                 ngram_counter[ngram] += 1
@@ -70,16 +70,23 @@ def get_ngram_counter(docs, min_count=10, n_range=(1,3)):
 
     return ngram_counter
 
+komoran = Komoran()
 
 # data preprocessing
 
 os.chdir(r"C:\analytics")
 
+stopWord = pd.read_csv("dataset7.stopwords.csv", names=["stopword"])
+stopwords = stopWord.stopword.to_list()
+
 df = pd.read_csv("auditingStandard.csv", encoding="cp949", names=('documents', 'B'))
 df = df.loc[:, ~df.columns.str.contains('B')]  # 제거
 df = df.dropna()  # Nan 제거
 
-df["documents"] = doPreprocess(df["documents"])
+df["documents"] = doPreprocess(df["documents"], 
+                               komoran.nouns, 
+                               stopwords,
+                               desc="Audit Standard")
 
 df["documents"].str.strip()  # Whitespace 제거
 df["count"] = df["documents"].str.len()  # string 수 
@@ -88,7 +95,10 @@ df = df[df["count"] > 3]  # 입력내용이 3을 초과하는 입력값
 df2 = pd.read_excel("dataset3.preprocessed(2017-2019).xlsx", sheet_name="data")
 df2 = df2.dropna()  # Nan 제거
 
-df2["documents"] = doPreprocess(df2["documents"])
+df2["documents"] = doPreprocess(df2["documents"], 
+                                komoran.nouns, 
+                                stopwords,
+                                desc="감사업무수행내역")
 
 df2["documents"].str.strip()  # Whitespace 제거
 df2["count"] = df2["documents"].str.len()  # string 수 
@@ -97,17 +107,24 @@ df2 = df2[df2["count"] > 3]  # 입력내용이 3을 초과하는 입력값
 df = df.append(df2)
 
 # N-gram 적용 토큰화
-
-komoran = Komoran()
-ngram_counter = get_ngram_counter(df["documents"], min_count=100, n_range=(2,5))  # 2-5 단어까지 추출
+ngram_counter = get_ngram_counter(df["documents"], 
+                                  komoran.nouns, 
+                                  min_count=10, 
+                                  n_range=(2,5))  # 2-5 단어까지 추출
 
 ngramList = []
 
-for key in ngram_counter.items():
-    # 전처리에서 명사만 추출하도록 변경        
+for key in tqdm(ngram_counter.items(), desc="ListOut"):       
     keyText = "-".join(key[0])
     keyText2 = keyText.replace("-", "")
-    ngramList.append([keyText, keyText2, key[1]])
+    keyText3 = keyText.replace("-", " ")
+    ngramList.append([keyText, keyText2, keyText3, key[1]])
 
-exportNgramDictionary = pd.DataFrame(ngramList)
-exportNgramDictionary.to_excel('Ngramdictionary.xlsx')
+NgramDictionary = pd.DataFrame(ngramList).sort_values(by=[3])
+
+NgramDictionary["tag"] = "NNP"
+NgramDictionary = NgramDictionary[[2, "tag"]]
+NgramDictionary.to_csv('Ngramdictionary.txt',    # 코모란 사용자 사전 
+                        sep='\t', 
+                        header=False,
+                        index=False)
